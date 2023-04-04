d2bbbe2ce5e8484acc95d20a349e60df19472855
Lummi <72337143+lummi01@users.noreply.github.com>
2023-04-04 20:23:31 +0000

    Add files via upload

--- asteroids.cpp
+++ asteroids.cpp
@@ -1,3 +1,5 @@
+#include <list>
+
 #include "asteroids.hpp"
 #include "assets.hpp"
 
@@ -60,12 +62,21 @@ struct UFO
     bool is;
     float x;
     float y;
-    float dx;
+    float dx = .2f;
+	float dy = .1f;
     float sx;
     float sy;
-    float sd;
+    float sdx;
+	float sdy;
     short st;
     short ani;
+};
+
+struct EXPLOSION
+{
+	Vec2 pos;
+	Vec2 vel;
+	short alpha;
 };
 
 SaveData saveData;
@@ -74,6 +85,8 @@ PLAYER p;
 SHOT shot[4];
 ASTEROID asteroid[54];
 UFO ufo;
+
+static std::list<EXPLOSION> particles;
 
 Tween tween_control;
 
@@ -99,22 +112,39 @@ void ani_update(Timer &t)
 
 void new_ufo(Timer &t)
 {
-    if (rand() %2 == 0)
-    {
-        ufo.x = 0;
-        ufo.dx = .2f;
-    }
-    else
-    {
-        ufo.x = SCREEN_WIDTH;
-        ufo.dx = -.2f;
-    }
     ufo.y = 4 + rand() %(SCREEN_HEIGHT - 8);
     ufo.sx = ufo.x;
     ufo.sy = ufo.y;
-    ufo.sd = rand ()%360 * PI;
-    ufo.st = 35;
+    ufo.st = 0;
     ufo.is = true;
+}
+
+void NewExplosion(float x, float y, short number)
+{
+	for(short n=0; n<number; n++) 
+	{
+	    EXPLOSION e;
+        float d = rand() %360 * PI;		
+        e.pos = Vec2(x, y);
+        e.vel = Vec2 (sin(d), cos(d)) * Vec2(.2f, .2f);
+        e.alpha = 50 + rand() %200;
+        particles.push_back(e);
+	}
+}
+
+void UpdateExplosion()
+{
+    for(auto e = particles.begin(); e != particles.end();) 
+	{
+        if(e->alpha < 5) 
+		{
+            e = particles.erase(e);
+            continue;
+        }
+        e->pos += e->vel;
+        e->alpha -= 5;
+        ++e;
+    }
 }
 
 void NewAsteroid(short type, float x, float y, float dx, float dy)
@@ -224,6 +254,14 @@ void UpdateControl()
     {
         p.dx += float(sin(p.sprite * 15.0f * PI)) * .01f;
         p.dy += float(-cos(p.sprite * 15.0f * PI)) * .01f;  
+
+	    EXPLOSION e;
+        float dx = float(-sin(p.sprite * 15.0f * PI)) * .1f;
+        float dy = float(cos(p.sprite * 15.0f * PI)) * .1f;  
+        e.vel = Vec2(dx, dy);
+        e.pos = Vec2(p.x, p.y) + (e.vel * Vec2(35, 35));
+        e.alpha = 150 + rand() %100;
+        particles.push_back(e);
     }
     else
     {
@@ -251,7 +289,7 @@ void UpdateControl()
     }
     if (buttons.pressed & Button::A)
         p.shot = true;
-
+/*
     if (buttons & Button::B) // teleport
     {
         p.is = false;
@@ -261,6 +299,7 @@ void UpdateControl()
         p.dx = 0;
         p.dy = 0;
     }
+*/
 }
 
 void AsteroidUpdate()
@@ -304,6 +343,7 @@ bool AsteroidCollision(float x, float y, short r, short score)
             {
                 if (r < 5)
                 {
+					NewExplosion(asteroid[a].x, asteroid[a].y, 12);
                     game.score += (40 / asteroid[a].type) * score;
                     if (asteroid[a].type > 2)
                         for (short i=0; i<3; i++)
@@ -334,8 +374,10 @@ void ShotCollision()
             }
             else if (ufo.is && Collision(shot[s].x, shot[s].y, 0, ufo.x, ufo.y, 3))
             {
+				NewExplosion(ufo.x, ufo.y, 16);
                 game.score += 50;
                 ufo.is = false;
+				ufo.dx > 0? ufo.x = 0: ufo.x = SCREEN_WIDTH;
                 ufo_timer.start();
                 shot[s].time = 0;
                 break;
@@ -352,17 +394,18 @@ bool PlayerCollision()
     {
         if (Collision(p.x, p.y, 3, ufo.x, ufo.y, 3))
         {
+			NewExplosion(p.x, p.y, 16);
+			NewExplosion(ufo.x, ufo.y, 16);
             game.score += 50;
             ufo.is = false;
+			ufo.dx > 0? ufo.x = 0: ufo.x = SCREEN_WIDTH;
             ufo_timer.start();
             return true;
         }
         else if (Collision(p.x, p.y, 3, ufo.sx, ufo.sy, 0))
         {
-            ufo.sx = ufo.x;
-            ufo.sy = ufo.y;
-            ufo.sd = rand ()%360 * PI;
-            ufo.st = 35;
+			NewExplosion(p.x, p.y, 16);
+            ufo.st = 0;
             return true;
         }
     }    
@@ -374,22 +417,54 @@ void UpdateUFO()
     if (ufo.is)
     {
         ufo.x+=ufo.dx;
-        if (ufo.x < 0 || ufo.x > SCREEN_WIDTH || AsteroidCollision(ufo.x, ufo.y, 3, 0))
+		ufo.y+=ufo.dy;
+        if (ufo.x < 0 || ufo.x > SCREEN_WIDTH)
         {
+			ufo.dx =- ufo.dx;
             ufo.is = false;
             ufo_timer.start();
         }
-        ufo.sx += float(sin(ufo.sd));
-        ufo.sy += float(-cos(ufo.sd));
+		else if (AsteroidCollision(ufo.x, ufo.y, 3, 0))
+		{
+			ufo.dx > 0? ufo.x = 0: ufo.x = SCREEN_WIDTH;
+            ufo.is = false;
+            ufo_timer.start();
+		}
+        if (ufo.y < 4 || ufo.y > SCREEN_HEIGHT - 4)
+			ufo.dy=-ufo.dy;
+
+        ufo.sx += ufo.sdx;//float(sin(ufo.sd));
+        ufo.sy += ufo.sdy;//float(-cos(ufo.sd));
         ufo.st--;
         if (ufo.st < 0 || AsteroidCollision(ufo.sx, ufo.sy, 0, 0))
         {
+
+			float dx = p.x - ufo.x;
+			float dy = p.y - ufo.y;
+			float hyp = 100;
+			if (p.is)
+				hyp = sqrt((dx * dx) + (dy * dy));
+			for (short a=0; a<54; a++)
+			{
+				if (asteroid[a].type > 0)
+				{
+					float adx = asteroid[a].x - ufo.x;
+					float ady = asteroid[a].y - ufo.y;
+					float ahyp = sqrt((adx * adx) + (ady * ady));
+					if (ahyp < hyp)
+					{
+						dx = adx;
+						dy = ady;
+						hyp = ahyp;
+					}
+				}
+			}
             ufo.sx = ufo.x;
             ufo.sy = ufo.y;
-            ufo.sd = rand ()%360 * PI;
-            ufo.st = SCREEN_HEIGHT / 2;
+			ufo.sdx = dx / hyp;
+			ufo.sdy = dy / hyp;
+            ufo.st = 45;
         }
-        
     }
 }
 
@@ -478,6 +553,13 @@ void render(uint32_t time)
                 screen.sprite(136 + asteroid[a].ani + (asteroid[a].color * 16), Point(asteroid[a].x - 4, asteroid[a].y - 4));
         }
 
+	    for(auto &p : particles)
+		{
+			screen.alpha = p.alpha;
+	        screen.pixel(Point(p.pos.x, p.pos.y));
+		}
+		screen.alpha = 255;
+
         if (ufo.is)
         {
             screen.pixel(Point(ufo.sx, ufo.sy)); //UFO shot
@@ -505,6 +587,7 @@ void render(uint32_t time)
         screen.text(score_txt + score, font, Point(1, SCREEN_HEIGHT -6), true, TextAlign::buttom_left);        
 */
         screen.text(std::to_string(game.score), font, Point(SCREEN_WIDTH, SCREEN_HEIGHT -6), true, TextAlign::top_right);
+//        screen.text(std::to_string(game.asteroid), font, Point(0, 0), true, TextAlign::top_left);
 
         if (game.state == 2)
             screen.text("game over", minimal_font, Point(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2), true, TextAlign::center_center);
@@ -517,6 +600,7 @@ void update(uint32_t time)
 {
     AsteroidUpdate();
     UpdateUFO();
+	UpdateExplosion();
 
     if (game.state == 0) //title
     {
@@ -532,7 +616,11 @@ void update(uint32_t time)
             {
                 p.is = false; //died
                 if (game.live < 1) //end of game
+				{
+					for (short s=0; s<4; s++)
+						shot[s].time = 0;
                     game.state = 2;
+				}
             }
         }
         else if(AsteroidCollision(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, 16, 0) == false && ufo.is == false)
@@ -550,7 +638,7 @@ void update(uint32_t time)
     }
     else if (game.state == 2) //game over
     {
-        if (buttons.released & Button::A)
+        if (buttons.released & Button::B)
             game.state = 0;        
     }
 }
